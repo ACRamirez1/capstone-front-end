b38b43e46f06bbcc291bb14554b256b4f3801703
ACRamirez1 <caramirez711@gmail.com>
2023-05-31 12:43:01 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from'react';
+import Api from './Api.js';
+import './App.css';
+
+
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <iframe title='test' width='600px' height='1000px' src= 'https://greatergood.berkeley.edu/article/item/what_is_your_purpose_as_a_father' />
+      <Api />
+      
+    </div>
+  );
+}
+
+export default App;
