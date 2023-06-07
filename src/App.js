b3bea6e94ca3d23d677000a9d4e18453b96fc35c
ACRamirez1 <caramirez711@gmail.com>
2023-06-07 00:32:58 -0500

    Added background Image with overlay

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import { useState } from "react";
 import Navigation from "./components/Navigation";
-import { BrowserRouter } from "react-router-dom";
-import Router from "./ApplicationRouter";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./components/LoginPage";
 import Quotes from "./components/Quotes";
 import { Box } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
+import Home from "./components/Home";
+import LoginPage from "./components/LoginPage";
 
 function App() {
   const [token, setToken] = useState("");
+  const isLoggedIn = token !== "";
   return (
     <div className="App">
-      <Login setToken={setToken} />
-      <Quotes token={token} />
-
+      {/* Navigation goes here */}
+      <Box style={{ backgroundColor: "blue", height: 20, width: "100%" }}></Box>
       <BrowserRouter>
-        <Router />
+        {!isLoggedIn ? (
+          <Routes>
+            <Route path="/" element={<LoginPage setToken={setToken} />} />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/" element={<Quotes token={token} />} />
+            <Route path="/quotes" element={<Quotes token={token} />} />
+            {/* Authenticated routes go here */}
+          </Routes>
+        )}
       </BrowserRouter>
     </div>
   );
--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,36 +1,47 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import logo from "./logo.jpg";
+import { Box, Image } from "@chakra-ui/react";
 
 const Quotes = (props) => {
-    console.log("is running");
-    const [quotes, setQutoes] = useState([]);
+  console.log("is running");
+  const [quotes, setQutoes] = useState([]);
 
-  
-    useEffect(() => {
-      axios
-        .get("https://capstone-ashen-zeta.vercel.app/quotes", {
-            headers: {
-                Authorization: `Bearer ${props.token}`
-            }
-        })
-        .then((response) => {
-            // console.log(response)
-          setQutoes(response.data)});
-    }, [props.token]);
+  useEffect(() => {
+    axios
+      .get("https://capstone-ashen-zeta.vercel.app/quotes", {
+        headers: {
+          Authorization: `Bearer ${props.token}`,
+        },
+      })
+      .then((response) => {
+        // console.log(response)
+        setQutoes(response.data);
+      });
+  }, [props.token]);
 
   return (
     <div>
-        <ul className='list'>
-            {!quotes.length ? (
-                <p>Loading quotes...</p>
-            ) : (
-                quotes.map(({id, quote, author}) => (
-                    <li className='singleQuote' key={id}>{quote}</li>
-                ))
-            )}
-        </ul>
-    </div>
-  )
-}
+        
+<Image src={logo} alt='Dan Abramov' style={{
+    position: 'absolute',
+    width: '100vw',
+    zIndex: -1
+}} />
 
-export default Quotes
+      <ul className="list">
+        {!quotes.length ? (
+          <p>Loading quotes...</p>
+        ) : (
+          quotes.map(({ id, quote, author }) => (
+            <li className="singleQuote" key={id}>
+              {quote}
+            </li>
+          ))
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default Quotes;
